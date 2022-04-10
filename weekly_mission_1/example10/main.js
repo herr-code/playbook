2e35bb63fd9bcbd21ff3ce18ccb844e9ff44dcb5
herr-code <herrwebj@gmail.com>
2022-04-09 20:57:11 -0500

    Ejercicios terminados

---
+++ weekly_mission_1/example10/main.js
@@ -0,0 +1,8 @@
+// Ejemplo de exportación usando ESM
+import MyPokemon from './pokemon.js'
+
+const pikachu = new MyPokemon('Pikachu')
+pikachu.sayHello()
+
+
+// Usar node --experimental-modules main.js 
---
+++ weekly_mission_1/example7/main_module.js
@@ -0,0 +1,7 @@
+/* Importando el módulo */
+
+// Como es node 12 se debe correr asi para que no de error
+// node --experimental-modules index.js 
+import * as loggerModule from './logger.js'
+
+console.log(loggerModule)
